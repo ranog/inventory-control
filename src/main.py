5615295be7626c6d71af7bb9758682b3327df4f3
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-03-11 23:18:57 -0300

    add endpoint for creating parts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from src.service import register_part
 
+from src.model import Part
 
 app = FastAPI()
 
@@ -7,3 +9,8 @@ app = FastAPI()
 @app.get('/v1/ping/', include_in_schema=False)
 async def root():
     return 'pong'
+
+
+@app.post('/v1/parts/')
+async def create_part(part: Part):
+    return await register_part(part)
---
+++ tests/e2e/test_main.py
@@ -0,0 +1,34 @@
+from httpx import AsyncClient
+import pytest
+from src.service import part_details
+from src.main import app
+
+
+@pytest.fixture
+async def async_http_client():
+    async with AsyncClient(app=app, base_url='http://test') as async_client:
+        yield async_client
+
+
+async def test_it_should_ping_successfully(async_http_client: AsyncClient):
+    response = await async_http_client.get('/v1/ping/')
+
+    assert response.status_code == 200
+    assert response.json() == 'pong'
+
+
+async def test_it_should_register_part(async_http_client: AsyncClient):
+    part_payload = {
+        'name': 'Stepper motor',
+        'quantity': 100,
+        'description': 'NEMA 17 stepper motor',
+    }
+
+    response = await async_http_client.post('/v1/parts/', json=part_payload)
+
+    expected_result = await part_details(response.json())
+
+    assert response.status_code == 200
+    assert part_payload['name'] == expected_result.name
+    assert part_payload['quantity'] == expected_result.quantity
+    assert part_payload['description'] == expected_result.description
