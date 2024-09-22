bdfaf50504fb342d4c34a5a7c3d025d7c818060d
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-09-21 22:47:08 -0300

    feat: implement service to add batch

--- src/v2/allocation/service_layer/services.py
+++ src/v2/allocation/service_layer/services.py
@@ -1,3 +1,7 @@
+from datetime import date
+from typing import Optional
+
+from src.v2.allocation.adapters import orm
 from src.v2.allocation.adapters.repository import AbstractRepository
 from src.v2.allocation.domain import model
 from src.v2.allocation.domain.model import Batch, OrderLine
@@ -11,10 +15,16 @@ def is_valid_sku(sku: str, batches: list[Batch]) -> bool:
     return sku in {batch.sku for batch in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(order_id: str, sku: str, qty: int, repo: AbstractRepository, session) -> str:
     batches = repo.list()
-    if not is_valid_sku(sku=line.sku, batches=batches):
-        raise InvalidSku(f'Invalid sku {line.sku}')
-    batch_ref = model.allocate(line=line, batches=batches)
+    if not is_valid_sku(sku=sku, batches=batches):
+        raise InvalidSku(f'Invalid sku {sku}')
+    batch_ref = model.allocate(line=OrderLine(order_id=order_id, sku=sku, qty=qty), batches=batches)
     session.commit()
     return batch_ref
+
+
+def add_batch(ref: str, sku: str, qty: int, eta: Optional[date], repo: AbstractRepository, session) -> None:
+    orm.create_tables()
+    repo.add(model.Batch(ref=ref, sku=sku, qty=qty, eta=eta))
+    session.commit()
--- tests/v2/unit/test_services.py
+++ tests/v2/unit/test_services.py
@@ -1,9 +1,13 @@
 import pytest
+from datetime import date, timedelta
 
 from src.v2.allocation.adapters import repository
-from src.v2.allocation.domain import model
 from src.v2.allocation.domain.model import Batch
 from src.v2.allocation.service_layer import services
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -27,31 +31,47 @@ class FakeSession:
         self.committed = True
 
 
+def test_add_batch():
+    repo, session = FakeRepository([]), FakeSession()
+
+    services.add_batch(ref='b1', sku='CRUNCHY-ARMCHAIR', qty=100, eta=None, repo=repo, session=session)
+
+    assert repo.get('b1') is not None
+    assert session.committed
+
+
 def test_returns_allocation():
-    line = model.OrderLine(order_id='o1', sku='RED-CHAIR', qty=3)
-    batch = model.Batch(ref='b1', sku='RED-CHAIR', qty=20, eta=None)
-    repo = FakeRepository([batch])
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch(ref='batch1', sku='RED-CHAIR', qty=20, eta=None, repo=repo, session=session)
 
-    result = services.allocate(line=line, repo=repo, session=FakeSession())
+    result = services.allocate(order_id='o1', sku='RED-CHAIR', qty=3, repo=repo, session=session)
 
-    assert result == 'b1'
+    assert result == 'batch1'
 
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine(order_id='o1', sku='NON-EXISTENT', qty=3)
-    batch = model.Batch(ref='b1', sku='AREALSKU', qty=20, eta=None)
-    repo = FakeRepository([batch])
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch(ref='batch1', sku='MAGICAL-CHALICE', qty=20, eta=None, repo=repo, session=session)
 
     with pytest.raises(services.InvalidSku, match='Invalid sku NON-EXISTENT'):
-        services.allocate(line=line, repo=repo, session=FakeSession())
+        services.allocate(order_id='o1', sku='NON-EXISTENT', qty=3, repo=repo, session=session)
 
 
 def test_commits():
-    line = model.OrderLine(order_id='o1', sku='OMINOUS-MIRROR', qty=3)
-    batch = model.Batch(ref='b1', sku='OMINOUS-MIRROR', qty=20, eta=None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch(ref='b1', sku='OMINOUS-MIRROR', qty=20, eta=None, repo=repo, session=session)
 
-    services.allocate(line=line, repo=repo, session=session)
+    services.allocate(order_id='o1', sku='OMINOUS-MIRROR', qty=3, repo=repo, session=session)
 
     assert session.committed is True
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch(ref='in-stock-batch', sku='RETRO-CLOCK', qty=100, eta=None, repo=repo, session=session)
+    services.add_batch(ref='shipment-batch', sku='RETRO-CLOCK', qty=100, eta=tomorrow, repo=repo, session=session)
+
+    services.allocate(order_id='order-123', sku='RETRO-CLOCK', qty=10, repo=repo, session=session)
+
+    assert repo.get('in-stock-batch').available_quantity == 90
+    assert repo.get('shipment-batch').available_quantity == 100
